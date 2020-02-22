45b4f36169135feb9cff35c8bdc1bf1fb6728a2e
Atul R <atulanand94@gmail.com>
2020-02-22 23:40:02 +0100

    cleanup

--- src/setup.ts
+++ src/setup.ts
@@ -7,10 +7,10 @@ import mkdirp from 'make-dir';
 
 type SetupOptions = {
   outDir: string;
+  id: string;
   downloadLink: string;
   cacheDir?: string;
   force?: boolean;
-  id: string;
   displayName?: string;
 };
 
